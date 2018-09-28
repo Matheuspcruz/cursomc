48e7b763c3500274e65a6b4a06b29364a5f5d4f4
Matheus Cruz <matheus.p.cruz@hotmail.com>
2018-09-28 17:12:29 -0300

    Intanciando na classe principal com commandLineRunner

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -1,12 +1,35 @@
-package com.nelioalves.cursomc;
+package com.nelioalves.cursomc;	
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner { //permitir implementar um metodo auxiliar para executar uma ação quando o program iniciar
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;//Dependencia 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// CommandLineRunner starta junto com a inicialização do programa
+		
+		Categoria cat1 = new Categoria(null,  "Informática");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		//Metodo salvAll salva as categorias no banco
+		//Array.asList salva mais de uma instancia 
 	}
 }
